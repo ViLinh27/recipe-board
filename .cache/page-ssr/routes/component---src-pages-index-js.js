faba778442dd96e30f5435f8d0a7146c145f2acb
Vi-Linh Nguyen <vilinh27@gmail.com>
2022-05-13 22:36:40 -0500

    moved a butt tone of stuff

---
+++ .cache/page-ssr/routes/component---src-pages-index-js.js
@@ -0,0 +1,49 @@
+exports.id = "component---src-pages-index-js";
+exports.ids = ["component---src-pages-index-js"];
+exports.modules = {
+
+/***/ "./src/pages/index.js":
+/*!****************************!*\
+  !*** ./src/pages/index.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles.css */ "./src/styles.css");
+/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_css__WEBPACK_IMPORTED_MODULE_1__);
+
+ // markup
+
+const IndexPage = () => {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("main", {
+    className: "pageStyles"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("title", null, "Home Page"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", {
+    className: "headingStyles"
+  }, "horizontal navbar here tabs with different sites"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", {
+    className: "paragraphStyles"
+  }, "intro blurb"));
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (IndexPage);
+
+/***/ }),
+
+/***/ "./src/styles.css":
+/*!************************!*\
+  !*** ./src/styles.css ***!
+  \************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=component---src-pages-index-js.js.map
---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+import "../styles.css"
+
+
+// markup
+const IndexPage = () => {
+  return (
+    <main className="pageStyles">
+      <title>Home Page</title>
+      <h1 className="headingStyles">
+       horizontal navbar here
+       tabs with different sites
+        
+      </h1>
+      <p className="paragraphStyles">
+        intro blurb
+      </p>
+      
+    </main>
+  )
+}
+
+export default IndexPage
