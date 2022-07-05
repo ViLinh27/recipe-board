96217c33bce12bb9562098f4b50296ac5534a343
Anh Nguyen <vianhnguyen17@gmail.com>
2022-07-05 11:54:48 -0500

    updating nav

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -5,7 +5,7 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import Link from 'gatsby';
 import "../../sassyStyles.css";
 
 const Navbar = () => {
