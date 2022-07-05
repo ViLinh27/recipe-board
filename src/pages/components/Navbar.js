1b792ed24a86755449695c524e1ed0ffee69aa13
Vi-Linh Nguyen <vilinh27@gmail.com>
2022-07-05 09:36:40 -0500

    change imports format

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {AppBar,Container, Toolbar,Box, useState} from '@material-ui/core';
+import {
+  AppBar,
+  Button,
+  Typography,
+  Container,
+  IconButton, 
+  Toolbar,
+  Box, 
+  useState} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import "../../sassyStyles.css";
