f66438a449930bca6b4735bcda63a2a434433748
Vi-Linh Nguyen <vilinh27@gmail.com>
2022-07-14 16:33:36 -0500

    add card to see what it looks like

--- src/pages/breakfast.js
+++ src/pages/breakfast.js
@@ -16,6 +16,8 @@ import {
   MenuItem, 
   useState} from '@material-ui/core';
 
+import Recipe from "./components/recipeCard";
+
 const Breakfast = () => {
     return(
        <main className="pageStyles">
@@ -34,6 +36,14 @@ const Breakfast = () => {
                 What should we eat in the morning? What's the most important meal of the day that would not make
                 anyone too sleepy on a workday?
             </Typography>
+
+            <Recipe
+                imageLink="https://assets.epicurious.com/photos/54ad4c516529d92b2c043ac5/master/w_960,c_limit/51200610_savory-oatmeal_1x1.jpg"
+                imageAlt="Savory Oatmeal with a Basted Egg"
+                cardTitle="Savory Oatmeal with a Basted Egg"
+                cardDescription=""
+                cardLink="https://www.epicurious.com/recipes/food/views/savory-oatmeal-with-a-basted-egg-51200610"
+            />
         </div>
        </main>
     )
